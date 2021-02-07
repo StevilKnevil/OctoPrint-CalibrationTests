de82e8468ce1c18c3774a26afdbaa939c39c0861
StevilKnevil <stevilknevil@hotmail.co.uk>
2021-02-07 20:21:26 +0000

    Added a view model for general settings for the overall calibration tests tabe

--- octoprint_calibrationtests/static/js/calibrationtests.js
+++ octoprint_calibrationtests/static/js/calibrationtests.js
@@ -11,3 +11,27 @@ $('#testSelector').on('change', function () {
         }
     });
 });
+
+
+$(function() {
+	function GeneralSettingsViewModel(parameters) {
+		var self = this;
+
+		self.settings = parameters[0];
+	}
+
+	// This is how our plugin registers itself with the application, by adding some configuration
+	// information to the global variable OCTOPRINT_VIEWMODELS
+	OCTOPRINT_VIEWMODELS.push({
+		// This is the constructor to call for instantiating the plugin
+		construct: GeneralSettingsViewModel,
+
+		// This is a list of dependencies to inject into the plugin, the order which you request
+		// here is the order in which the dependencies will be injected into your view model upon
+		// instantiation via the parameters argument
+		dependencies: ["settingsViewModel"],
+
+		// Finally, this is the list of selectors for all elements we want this view model to be bound to.
+		elements: ["#general_configuration"]
+	});
+});
