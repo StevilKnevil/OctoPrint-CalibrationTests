c187a12f59ea93473172890fd21a3e9342581b34
StevilKnevil <stevilknevil@hotmail.co.uk>
2021-02-23 20:14:32 +0000

    Update calibrationtests.js
    First pass of adding storable settings

--- octoprint_calibrationtests/static/js/calibrationtests.js
+++ octoprint_calibrationtests/static/js/calibrationtests.js
@@ -16,8 +16,32 @@ $('#testSelector').on('change', function () {
 $(function() {
 	function GeneralSettingsViewModel(parameters) {
 		var self = this;
-
 		self.settings = parameters[0];
+
+		self.isDirty = ko.observable(false);
+		self.saveSettings = function() {
+			// Save them
+			OctoPrint.settings.savePluginSettings("calibrationtests", 
+				{
+					confirmAllGcode: self.settings.settings.plugins.calibrationtests.confirmAllGcode(),
+					hotEndTemp: self.settings.settings.plugins.calibrationtests.hotEndTemp(),
+				}, null);
+			self.isDirty(false);
+		};	
+		self.resetSettings = function() {
+			alert("TODO")
+		}
+
+		self.onBeforeBinding = function() {
+			// TODO: This shouldn't update unless the settings are changed from here. I.e. if the user changes a setting in a different dialog, then this button shouldn't become active.
+			// Add a layer of indirection here?
+			self.settings.settings.plugins.calibrationtests.confirmAllGcode.subscribe(function () {
+				self.isDirty(true);
+			});
+			self.settings.settings.plugins.calibrationtests.hotEndTemp.subscribe(function () {
+				self.isDirty(true);
+			});
+		}
 	}
 
 	// This is how our plugin registers itself with the application, by adding some configuration
